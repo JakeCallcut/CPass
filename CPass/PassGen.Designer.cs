e650a8cead961aa75b20323302ca30f1f41a579a
Jake Callcut <jakecallcut123@gmail.com>
2021-06-17 08:08:21 +0100

    added password generator GUI

---
+++ CPass/PassGen.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CPass
+{
+    partial class PassGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPass = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.optCapitals = new System.Windows.Forms.CheckBox();
+            this.optLower = new System.Windows.Forms.CheckBox();
+            this.optNumbers = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.barLength = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Consolas", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(110, 39);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(196, 47);
+            this.lblPass.TabIndex = 0;
+            this.lblPass.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.barLength);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.optNumbers);
+            this.groupBox1.Controls.Add(this.optLower);
+            this.groupBox1.Controls.Add(this.optCapitals);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(22, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 323);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // optCapitals
+            // 
+            this.optCapitals.AutoSize = true;
+            this.optCapitals.Location = new System.Drawing.Point(11, 31);
+            this.optCapitals.Name = "optCapitals";
+            this.optCapitals.Size = new System.Drawing.Size(80, 27);
+            this.optCapitals.TabIndex = 0;
+            this.optCapitals.Text = "A - Z";
+            this.optCapitals.UseVisualStyleBackColor = true;
+            // 
+            // optLower
+            // 
+            this.optLower.AutoSize = true;
+            this.optLower.Location = new System.Drawing.Point(11, 70);
+            this.optLower.Name = "optLower";
+            this.optLower.Size = new System.Drawing.Size(78, 27);
+            this.optLower.TabIndex = 1;
+            this.optLower.Text = "a - z";
+            this.optLower.UseVisualStyleBackColor = true;
+            // 
+            // optNumbers
+            // 
+            this.optNumbers.AutoSize = true;
+            this.optNumbers.Location = new System.Drawing.Point(11, 112);
+            this.optNumbers.Name = "optNumbers";
+            this.optNumbers.Size = new System.Drawing.Size(77, 27);
+            this.optNumbers.TabIndex = 2;
+            this.optNumbers.Text = "0 - 9";
+            this.optNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 155);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 27);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "!@#$%^&*";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // barLength
+            // 
+            this.barLength.Location = new System.Drawing.Point(6, 233);
+            this.barLength.Name = "barLength";
+            this.barLength.Size = new System.Drawing.Size(191, 69);
+            this.barLength.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Length";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGen.Location = new System.Drawing.Point(254, 127);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(144, 85);
+            this.btnGen.TabIndex = 2;
+            this.btnGen.Text = "Regenerate Password";
+            this.btnGen.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(254, 218);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(144, 79);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy Password";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(254, 360);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(144, 78);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // PassGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblPass);
+            this.Name = "PassGen";
+            this.Text = "CPass - Password Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar barLength;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox optNumbers;
+        private System.Windows.Forms.CheckBox optLower;
+        private System.Windows.Forms.CheckBox optCapitals;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
--- CPass/Vault.cs
+++ CPass/Vault.cs
@@ -20,43 +20,7 @@ namespace CPass
 
         private void Vault_Load(object sender, EventArgs e)
         {
-            string path = @"f:\Github repos\CPass\CPass\Dependencies\Accounts.txt\";
-            if (!File.Exists(path))
-            {
-                using (StreamReader sr = File.OpenText(path))
-                {
-                    string s;
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        string title = "";
-                        for (int i = 0; i < s.Length; i++)
-                        {
-                            if (s.Substring(i, 1) == ",")
-                            {
-                                i = s.Length;
-                            }
-                            else
-                            {
-                                title = title + s.Substring(i, 1);
-                            }
-                        }
-                        passlist.Items.Add(title);
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("Accounts.txt is missing", "Critical Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
 
-            using (StreamReader sr = File.OpenText(path))
-            {
-                string s;
-                while ((s = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(s);
-                }
-            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -66,7 +30,8 @@ namespace CPass
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            var _passGen = new PassGen();
+            _passGen.Show();
         }
     }
 }
